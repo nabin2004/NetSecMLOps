b5e5fa89e3fcdbccdccff42139111d984bf9bb6b
nabin2004 <olinabin2004@gmail.com>
2025-08-11 21:55:36 +0545

    Data ingestion component completed and also tested

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -14,9 +14,9 @@ class TrainingPipelineConfig:
         self.timestamp: str = timestamp
 
 class DataIngestionConfig:
-    def __init__(self, training_pipeline_config: TrainingPipelineConfig):
-        self.data_ingestion_dir: str = os.path.join(
-            training_pipeline_config.artifact_dir, training_pipeline.DATA_INGESTION_DIR_NAME
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.data_ingestion_dir:str=os.path.join(
+            training_pipeline_config.artifact_dir,training_pipeline.DATA_INGESTION_DIR_NAME
         )
         self.feature_store_file_path: str = os.path.join(
                 self.data_ingestion_dir, training_pipeline.DATA_INGESTION_FEATURE_STORE_DIR, training_pipeline.FILE_NAME
