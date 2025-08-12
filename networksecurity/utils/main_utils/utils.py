037755a534ca9fcd23ae2b6e70143e8453160d85
nabin2004 <olinabin2004@gmail.com>
2025-08-12 18:42:54 +0545

    Added Data Transformation step

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -4,6 +4,7 @@ from networksecurity.logging.logger import logging
 import os, sys 
 # import dill 
 import pickle
+import numpy as np
 
 def read_yaml_file(file_path: str) -> dict:
     try:
@@ -25,3 +26,27 @@ def write_yaml_file(file_path: str, content: object, replace: bool = False) -> N
     
     except Exception as e:
         raise NetworkSecurityException(e, sys)
+    
+def save_numpy_array_data(file_path: str, array: np.array):
+    """
+    Save numpy array data to file
+    file_path: str location of file to save
+    array: np.array data to save
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e
+
+def save_object(file_path: str, obj: object) -> None:
+    try:
+        logging.info("Entered the save_object method of MainUtils class")
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+        logging.info("Exited the save_object method of MainUtils class")
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e
